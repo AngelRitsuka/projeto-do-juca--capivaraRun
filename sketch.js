07ccebb1147ab0168b5875d749dbcf1cecfa8d0f
AngelRitsuka <88597612+AngelRitsuka@users.noreply.github.com>
2023-01-31 17:17:28 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,114 @@
+//inclui os estados de jogo, que ai se quiser adicionar funcionalidades, tu vai conseguir 
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var character, backgroundimg, walking_animation;
+var groundimg, ground, lado1, lado2, lado3, lado4;
+
+function preload()
+{
+    walking_animation = loadAnimation("tile000.png","tile001.png","tile002.png","tile003.png");
+
+    backgroundimg = loadImage("fundo.png");
+    groundimg = loadImage("piso.png");
+}
+
+function setup()
+{
+    createCanvas(windowWidth,windowHeight);
+    
+    //lados estão com bons limites agora ;) só precisava antes mexer no lado 3 e 4 no eixo x
+    //cima e baixo
+    lado1=createSprite(width*0.01, height*0.01, width, height*0.01); //x,y,w,h
+    lado2=createSprite(width*0.01, height*0.99, width, height*0.01);
+    //esquerda e direita
+    lado3=createSprite(width*0.01, height, width*0.01, height);
+    lado4=createSprite(width*0.99,   height,   width*0.01, height);
+    
+
+    background=createSprite(width/2, height/3);
+    background.addImage(backgroundimg);
+    background.scale = 2.7;
+
+    character=createSprite(width/2, height/2);
+    character.addAnimation("walking", walking_animation);
+    character.scale = 1.7;
+
+
+    ground=createSprite(width/2,height/1.1);
+    ground.addImage(groundimg);
+    ground.velocityX = -5;
+    ground.scale = 0.2  ;
+    character.debug = true
+}
+
+function draw()
+{
+
+    character.collide(ground);
+    character.collide(lado1);
+    character.collide(lado2);
+    character.collide(lado3);
+    character.collide(lado4);
+
+
+    if(gameState === PLAY)
+    {
+        /*apenas retirei a velocity a mais que tinha 
+        e modifiquei o percentual da tela, na metade da
+        tela, a "capibara-san" estava quase tocando no chao com
+        o pulo infinito kkk então fui ajustando apenas ^_^
+        Ah! E quanto a gravidade também depois se quiser
+        ajustar novamente e deixar leve, pode deixar, mas
+        só que tem um porém :C :
+        quanto mais tu deixar a char leve mais dificil pra ela
+        cair será, então ela vai acabar caindo devagar (como
+            se estivesse flutuando sabe?)
+            Adicionei também uns gameStates pra te ajudar 
+            a colocar outras coisas no game ^^
+            Qualquer coisa só dizer.
+        */
+        if (keyDown("space") && character.y>=height/1.75)
+        {
+        character.velocityY = -15;
+        }
+        //gravidade
+        character.velocityY = character.velocityY + 1;
+
+        //controle
+        if (keyDown("a"))
+        {
+        character.x = character.x -19;
+        }
+
+        if (keyDown("d"))
+        {
+        character.x = character.x +19;
+        }
+
+  
+        //chão infinito
+        if(ground.x < width/2.65)
+        {
+        ground.x = width/2;
+        }
+       
+        
+        //character.y>=height*0.59
+
+
+    }
+
+    if(gameState === END)
+    {
+
+
+
+    }
+
+
+
+    drawSprites();
+
+}
